04bb19c08ad4c088ee3483ac8123189769f55c84
zzzzzzzs <55184223+ZzzzzzzSkyward@users.noreply.github.com>
2021-04-05 23:25:32 +0800

    Update tieba.py

--- batch/tieba.py
+++ batch/tieba.py
@@ -174,6 +174,7 @@ def main():
         tbs = get_tbs(i)
         favorites = get_favorite(i)
         for j in favorites:
+            logger.info(j)
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
     logger.info("所有用户签到结束")
