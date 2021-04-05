76058a8adbf7a27d35efd1266c5091b428b7bd2f
zzzzzzzs <55184223+ZzzzzzzSkyward@users.noreply.github.com>
2021-04-05 23:20:57 +0800

    Update main.py

--- batch/main.py
+++ batch/main.py
@@ -10,6 +10,7 @@ def to_file(src):
 for i in script_list:
     print(to_file(i))
     try:
+        os.system("chmod 766 ./batch/*")
         os.system(to_file(i))
     except Exception as e:
         print(e)
