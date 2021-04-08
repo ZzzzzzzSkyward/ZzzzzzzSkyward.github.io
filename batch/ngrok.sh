64699fe5cfaef95ea6fa819344539c9523df166f
zzzzzzzs <55184223+ZzzzzzzSkyward@users.noreply.github.com>
2021-04-08 23:35:21 +0800

    Update ngrok.sh

--- batch/ngrok.sh
+++ batch/ngrok.sh
@@ -1,4 +1,4 @@
-wget -q https://www.ngrok.cc/sunny/linux_amd64.zip?v=2.1
+wget -q -O linux_amd64.zip https://www.ngrok.cc/sunny/linux_amd64.zip?v=2.1
 unzip linux_amd64.zip
 chmod +x ./sunny
 ./sunny 127abbc4d7494185
